f3f2ed0271e6b918c1bafb28f9627f372244e829
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-12-23 14:50:19 +0200

    Extractor Path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{Json, Router, routing::{get}};
 use serde::Serialize;
+use axum::extract::Path;
 
 #[derive(Serialize)]
 struct Message {
@@ -16,10 +17,15 @@ async fn create_items() -> Json<Message> {
     Json( Message { text: "Creating an item".to_string() })
 }
 
+async fn get_user(Path(user_id): Path<i32>) -> Json<Message> {
+    Json( Message { text: format!("Welcome user id: {}", user_id) })
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-                    .route("/", get(get_items).post(create_items));
+                    .route("/", get(get_items).post(create_items))
+                    .route("/users/{user_id}", get(get_user));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
