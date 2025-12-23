eef2700389b531a59a6cab52593a2d20cd23bde4
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-12-23 15:14:52 +0200

    more routing nested

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use axum::{Json, Router, routing::{get}};
-use serde::Serialize;
+use axum::{Json, Router, routing::{get, post}};
+use serde::{Deserialize, Serialize};
 use axum::extract::Path;
 
 #[derive(Serialize)]
 struct Message {
     text: String,
 }
+
+#[derive(Serialize, Deserialize)]
+struct Output {
+    result: i32,
+}
+
 
 async fn get_items() -> Json<Message> {
     Json(Message {
@@ -21,15 +27,34 @@ async fn get_user(Path(user_id): Path<i32>) -> Json<Message> {
     Json( Message { text: format!("Welcome user id: {}", user_id) })
 }
 
+async fn greet(Path(name): Path<String>) -> String {
+    format!("Hello, and welcome {}", name)
+}
+
+async fn square(Path(n): Path<i32>) -> Json<Output> {
+    Json(Output { result: n * 2 })
+}
+
 #[tokio::main]
 async fn main() {
 
+    //Route for Users
     let user_routes = Router::new()
                     .route("/users", get(get_items).post(create_items))
                     .route("/users/{user_id}", get(get_user));
+
+    //Route for greetings
+    let greeting_routes = Router::new()
+    .route("/greet/{name}", get(greet));
+
+    //Route for square values
+    let square_routes = Router::new()
+    .route("/{n}", post(square));
     //Nesting users under api path.
     let app = Router::new()
-                    .nest("/api", user_routes);
+                    .nest("/welcome", greeting_routes)
+                    .nest("/api", user_routes)
+                    .nest("/calculate-square", square_routes);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
