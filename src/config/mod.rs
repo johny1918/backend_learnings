54c9f2b44afb95421e4565a8c0f672b0b31c2cca
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-12-29 11:45:51 +0200

    fix error related to JWT env

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,12 +1,12 @@
 use std::env;
 
-use crate::models::config::AppConfig;
+use crate::models::config::{AppConfig, Secret};
 
 impl AppConfig {
     pub fn from_env() -> Self {
         Self {
             database_url: env::var("DATABASE_URL").expect("DATABASE_URL is required"),
-            jwt_secret: env::var("JWT_SECRET").expect("JWT_SECRET is required"),
+            jwt_secret: Secret(env::var("JWT_SECRET").expect("JWT_SECRET is required")),
             port: env::var("PORT").unwrap_or_else(|_|"3000".into()).parse().unwrap(),
         }   
     }
